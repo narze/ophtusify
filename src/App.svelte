c32f428dddfcf3ca056bd1057924a845eb753cd9
Manassarn Manoonchai <manassarn@gmail.com>
2022-05-12 22:41:14 +0700

    Upload image

--- src/App.svelte
+++ src/App.svelte
@@ -2,21 +2,37 @@
   let title = "Ophtus Post Fan Remake!"
   let description =
     "ไม่ต้องรอแฟนเพจ ชาวเน็ตออกโรงเอง ฟอนต์ใหญ่ทะลุจอ ไม่ต้องจ้องให้เมื่อยตา!"
+
+  let avatar, fileinput
+
+  const onFileSelected = (e) => {
+    let image = e.target.files[0]
+    let reader = new FileReader()
+    reader.readAsDataURL(image)
+    reader.onload = (e) => {
+      avatar = e.target.result
+    }
+  }
 </script>
 
-<main class="bg-gray-800 min-h-screen grid place-content-center">
+<main class="bg-gray-800 min-h-screen grid place-content-center gap-8">
   <h1 class="text-6xl text-white text-center uppercase">Ophtusify</h1>
 
-  <div class="bg relative h-[600px] w-[600px] overflow-hidden mt-8">
+  <div class="bg relative h-[600px] w-[600px] overflow-hidden mt-4">
     <div class="relative m-[2.5%] h-[95%] w-[95%] overflow-hidden">
       <div
         class="absolute -top-[15px] -left-[15px] h-[600px] w-[600px] rotate-45 scale-[1.3] overflow-hidden bg-black"
       >
-        <img
-          src="https://cdn.discordapp.com/attachments/826099394205843533/973791884331847720/Screenshot_210.png"
-          class="absolute -rotate-45 -left-[5%] top-[14%]"
-          alt=""
-        />
+        <div class="absolute h-full w-full -rotate-45">
+          <div class="absolute top-20 left-20 right-20 bottom-0">
+            <img
+              class="object-cover w-full h-full"
+              src={avatar ||
+                "https://cdn.discordapp.com/attachments/826099394205843533/973791884331847720/Screenshot_210.png"}
+              alt=""
+            />
+          </div>
+        </div>
         <div
           class="bg-gradient absolute left-24 bottom-0 h-1/2 w-full -rotate-45"
         />
@@ -42,6 +58,35 @@
       </p>
     </div>
   </div>
+
+  <input
+    class="bg-gray-200 font-bold text-[#6215f1] text-xl appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-[#6215f1]"
+    type="text"
+    bind:value={title}
+  />
+
+  <textarea
+    rows="2"
+    class="bg-gray-200 text-lg appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-[#6215f1]"
+    type="text"
+    bind:value={description}
+  />
+
+  <div
+    class="text-white text-center text-xl border rounded px-2 py-4"
+    on:click={() => {
+      fileinput.click()
+    }}
+  >
+    เลือกภาพ
+  </div>
+  <input
+    style="display:none"
+    type="file"
+    accept=".jpg, .jpeg, .png"
+    on:change={(e) => onFileSelected(e)}
+    bind:this={fileinput}
+  />
 </main>
 
 <style>
