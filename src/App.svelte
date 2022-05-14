5eaa88ee5189c9e05fe0088a59dc2c7ecaba9504
Manassarn Manoonchai <manassarn@gmail.com>
2022-05-14 17:53:13 +0700

    feat: improve UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { saveAs } from "file-saver"
-  import { copyImageToClipboard } from 'copy-image-clipboard'
+  import { copyImageToClipboard } from "copy-image-clipboard"
 
   import logo from "./assets/ophtus-logo.png"
   import placeholder from "./assets/9arm.jpg"
@@ -17,17 +17,15 @@
 
   let avatar, fileinput, node, imageWidth, avatarElm
 
-  let default_size = 30
-  let limit_min_size = 20
-
-  let isCopy = false 
-
-  let zoom = 100
+  let descriptionSize = 31
+  let isCopy = false
+  let saving = false
+  let imageZoom = 100
 
   const onAvatarLoad = () => {
     imageWidth = avatarElm.width
-    console.dir(avatarElm)
-  };
+    // console.dir(avatarElm)
+  }
 
   onMount(async () => {
     dragElement(avatarElm)
@@ -39,15 +37,17 @@
     reader.readAsDataURL(image)
     reader.onload = (e) => {
       avatar = e.target.result
-      setTimeout(() => onAvatarLoad(), 500);
+      setTimeout(() => onAvatarLoad(), 500)
     }
   }
 
   const download = () => {
+    saving = true
     domtoimage
       .toPng(node)
       .then(function (blob) {
         saveAs(blob, "ophtusify.png")
+        saving = false
       })
       .catch(function (error) {
         console.error("oops, something went wrong!", error)
@@ -55,20 +55,22 @@
   }
 
   function Copy() {
+    saving = true
     domtoimage
       .toPng(node)
       .then(function (dataUrl) {
-        let img = new Image();
-        img.src = dataUrl;
+        let img = new Image()
+        img.src = dataUrl
         copyImageToClipboard(img.src)
+        saving = false
       })
       .catch(function (error) {
         console.error("oops, something went wrong!", error)
       })
+    isCopy = !isCopy
+    setTimeout(() => {
       isCopy = !isCopy
-      setTimeout(() => {
-        isCopy = !isCopy
-      }, 5000);
+    }, 5000)
   }
 </script>
 
@@ -104,7 +106,8 @@
               bind:this={avatarElm}
               on:load={() => onAvatarLoad()}
               class="absolute top-0 left-0 max-w-none h-full cursor-move"
-              style="transform: scale({zoom / 100}); left:calc(50% - {imageWidth / 2}px)"
+              style="transform: scale({imageZoom /
+                100}); left:calc(50% - {imageWidth / 2}px)"
               src={avatar || placeholder}
               alt=""
             />
@@ -119,53 +122,83 @@
       <img src={logo} class="m-8 w-40" alt="" />
     </div>
     <div
-      class="absolute h-1/4 bottom-[2.5%] left-[2.5%] right-[2.5%] text-white mx-4 pointer-events-none"
+      class="absolute h-1/4 bottom-[2.5%] left-[2.5%] right-[2.5%] text-white mx-4 "
     >
       <!-- <h1
         class="text-4xl mb-2 font-bold text-transparent bg-clip-text bg-gradient-to-br from-[#6215f1] to-[#1b3efa]"
       >
         {title}
       </h1> -->
-      <h1 class="text-4xl mb-2 font-bold text-[#6215f1]">
+      <!-- <h1 class="text-4xl mb-2 font-bold text-[#6215f1]">
         {title}
-      </h1>
-      <p style={`font-size: ${default_size}px;`} class={`text-white font-medium`}>
+      </h1> -->
+
+      <!-- svelte-ignore a11y-autofocus -->
+      <input
+        class={`${
+          saving
+            ? "border-2 border-transparent resize-none"
+            : "border-2 border-[#6215f188] rounded-xl"
+        } caret-white absolute -top-2 bg-transparent text-4xl font-bold text-[#6215f1] appearance-none rounded w-full leading-tight focus:outline-none focus:border-[#6215f1]`}
+        type="text"
+        autofocus={true}
+        bind:value={title}
+      />
+      <!-- <p
+        style={`font-size: ${descriptionSize}px;`}
+        class={`text-white font-medium`}
+      >
         {description}
-      </p>
+      </p> -->
+
+      <textarea
+        rows="2"
+        style={`font-size: ${descriptionSize}px;`}
+        class={`${
+          saving
+            ? "border-2 border-transparent resize-none overflow-y-hidden"
+            : "border-2 border-[#6215f188] rounded-xl"
+        } absolute top-10 bg-transparent appearance-none h-28 rounded w-full text-white leading-tight focus:outline-none focus:border-[#6215f1] overflow-clip"`}
+        type="text"
+        bind:value={description}
+      />
     </div>
   </div>
 
-  <div class="overflow-hidden mb-2">
+  <p class="text-white text-center">
+    วิธีใช้: แก้ไขคำตามใจชอบ เลื่อนภาพด้วยการคลิกที่ภาพแล้วลาก
+  </p>
+
+  <div class="flex gap-8 overflow-hidden text-white items-center">
+    <p class="text-xl w-1/3">ขนาดภาพ</p>
     <input
       type="range"
       min="50"
       max="200"
-      bind:value={zoom}
+      bind:value={imageZoom}
       class="slider"
     />
   </div>
 
-  <input
-    class="bg-black font-bold text-[#6215f1] text-center text-xl appearance-none border-2 border-gray-700 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:border-[#6215f1]"
-    type="text"
-    bind:value={title}
-  />
-
-  <textarea
-    rows="2"
-    class="bg-black font-medium text-center text-lg appearance-none border-2 border-gray-700 rounded w-full py-2 px-4 text-white leading-tight focus:outline-none focus:border-[#6215f1]"
-    type="text"
-    bind:value={description}
-  />
+  <div class="flex gap-8 overflow-hidden text-white items-center">
+    <p class="text-xl w-1/3">ขนาดตัวหนังสือ</p>
+    <input
+      type="range"
+      min="20"
+      max="80"
+      bind:value={descriptionSize}
+      class="slider"
+    />
+  </div>
 
   <div class="flex flex-row gap-2">
     <button
-      class="text-white  text-center text-xl border-2 border-slate-400 rounded px-2 py-4 basis-full bg-gradient-to-r from-[#6215f1] to-[#1b3efa] hover:border-[#6215f1]"
+      class="text-white text-center text-xl border-2 border-slate-400 rounded px-2 py-4 basis-full bg-gradient-to-r from-[#6215f1] to-[#1b3efa] hover:border-[#6215f1]"
       on:click={() => {
         fileinput.click()
       }}
     >
-      เลือกภาพ
+      อัปโหลดภาพ
     </button>
     <input
       style="display:none"
@@ -185,29 +218,8 @@
     <button
       class="text-white text-center text-xl border-2 border-slate-400 rounded px-2 py-4 basis-full bg-gradient-to-r from-[#6215f1] to-[#1b3efa] hover:border-[#6215f1]"
       on:click={Copy}
-      >
-      {isCopy ? 'Copied' : 'Copy' }
-    </button>
-  </div>
-
-  <div class="flex flex-row gap-2">
-    <button
-      class="text-white  text-center text-xl border-2 border-slate-400 rounded px-2 py-4 basis-full bg-gradient-to-r from-[#6215f1] to-[#1b3efa] hover:border-[#6215f1]"
-      on:click={() => {
-        default_size += 2
-      }}
     >
-        เพิ่มขนาด
-    </button>
-    <button
-      class={`text-center text-xl border-2 border-slate-400 rounded px-2 py-4 basis-full bg-gradient-to-r ${default_size > limit_min_size ? "from-[#6215f1] to-[#1b3efa] text-white hover:border-[#6215f1]" : "from-[#6215f1]/50 to-[#1b3efa]/50 text-[#9f9f9f]"}  `}
-      on:click={() => {
-        if (default_size > limit_min_size) {
-            default_size -= 2
-        }
-      }}
-    >
-        ลดขนาด
+      {isCopy ? "Copied" : "คัดลอก"}
     </button>
   </div>
 </main>
